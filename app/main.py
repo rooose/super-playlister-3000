93747a87afef54396afe2cd1aad2d50220d17f2b
Rose Hirigoyen <rosehirigoyen@gmail.com>
2021-11-18 13:42:04 -0500

    auth working but not optimal

--- app/main.py
+++ app/main.py
@@ -1,6 +1,49 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, g
 from flask_cors import CORS
 
+from .config import CONFIG
+from .spotify_auth import *
+from functools import wraps
+
+# TODO: Use Flask sessions
+app = Flask(__name__)
+CORS(app)
+
+spotify_helper = SpotifyAuth(CONFIG)
+
+@app.route("/login", methods=['GET'])
+def login():
+   return render_template('login.html')
+
+
+@app.route("/auth")
+def spotify_auth():
+   return redirect(spotify_helper.auth_str)
+
+
+@app.route('/callback/')
+def callback():
+   spotify_helper.getUserToken(request.args['code'])
+   return redirect('/')
+
+
+def authenticated_resource(f):
+   @wraps(f)
+   def decorated(*args, **kwargs):
+      print(g)
+      if g.user is not None or spotify_helper.is_authenticated:
+         return f(*args, **kwargs)
+
+      return redirect('/login')
+
+   return decorated
+
+
 @app.route("/", methods=['GET'])
-def home_view():
-    return render_template('index.html', condition=False, error='', translation='', question='')
+@authenticated_resource
+def home():
+   if spotify_helper.needs_refresh:
+      spotify_helper.refresh_token()
+
+   return render_template('home.html')
+
--- app/spotify_auth.py
+++ app/spotify_auth.py
@@ -1,46 +1,85 @@
-import base64, json, requests
-
 # inspired from https://github.com/vanortg/Flask-Spotify-Auth
+import base64, json, requests
+from enum import Enum
+import time
 
-SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
-SPOTIFY_URL_TOKEN = 'https://accounts.spotify.com/api/token/'
-RESPONSE_TYPE = 'code'   
-HEADER = 'application/x-www-form-urlencoded'
-REFRESH_TOKEN = ''
-    
-def getAuth(client_id, redirect_uri, scope):
-    data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, client_id, redirect_uri, scope) 
-    return data
+class Token_Data(Enum):
+    ACCESS_TOKEN = 0
+    AUTHENTICATION_HEADER = 1
+    AUTHORIZED_SCOPES = 2
+    EXPIRATION = 3
 
 
-def getToken(code, client_id, client_secret, redirect_uri):
-    body = {
-        "grant_type": 'authorization_code',
-        "code" : code,
-        "redirect_uri": redirect_uri,
-        "client_id": client_id,
-        "client_secret": client_secret
-    }
+class SpotifyAuth():
+    SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
+    SPOTIFY_URL_TOKEN = 'https://accounts.spotify.com/api/token/'
+    RESPONSE_TYPE = 'code'
+    HEADER = 'application/x-www-form-urlencoded'
+
+    def __init__(self, config):
+        self.refresh_token = ''
+        self.token_data = []
+
+        self.config = config
+
+    @property
+    def is_authenticated(self):
+        return len(self.token_data) != 0 and time.time() < self.token_data[Token_Data.EXPIRATION.value]
+
+    @property
+    def needs_refresh(self):
+        return len(self.token_data) != 0 and time.time() >= self.token_data[Token_Data.EXPIRATION.value]
+
+    @property
+    def redirect_uri(self):
+        return f"{self.config['callback_url']}:{self.config['port']}/callback/"
+
+    @property
+    def auth_str(self): # vas getUser
+        return f"{SpotifyAuth.SPOTIFY_URL_AUTH}client_id={self.config['client_id']}&response_type=code&redirect_uri={self.redirect_uri}&scope={self.config['scope']}"
+
+    def handleToken(self, response):
+        print("RESPONSE: ", response)
+        auth_head = {"Authorization": f"Bearer {response['access_token']}"}
+        self.refresh_token = response["refresh_token"]
+        return [response["access_token"], auth_head, response["scope"], time.time() + response["expires_in"]]
+
+
+    def getToken(self, code):
+        body = {
+            "grant_type": 'authorization_code',
+            "code" : code,
+            "redirect_uri": self.redirect_uri,
+            "client_id":  self.config['client_id'],
+            "client_secret": self.config['client_secret']
+        }
+
+        to_encode = "{}:{}".format(self.config['client_id'], self.config['client_secret'])
+        encoded = base64.urlsafe_b64encode(to_encode.encode()).decode()
+        headers = {"Content-Type" : SpotifyAuth.HEADER, "Authorization" : f"Basic {encoded}"} 
+
+        post = requests.post(SpotifyAuth.SPOTIFY_URL_TOKEN, params=body, headers=headers)
+        return self.handleToken(json.loads(post.text))
+
+
+    def getUserToken(self, code):
+        self.token_data = self.getToken(code)
+
+
+    def refreshAuth(self):
+        body = {
+            "grant_type" : "refresh_token",
+            "refresh_token" : self.refresh_token
+        }
+
+        post_refresh = requests.post(SpotifyAuth.SPOTIFY_URL_TOKEN, data=body, headers=SpotifyAuth.HEADER)
+        payback = json.dumps(post_refresh.text)
         
-     
-    encoded = base64.b64encode("{}:{}".format(client_id, client_secret))
-    headers = {"Content-Type" : HEADER, "Authorization" : "Basic {}".format(encoded)} 
+        return self.handleToken(payback)
 
-    post = requests.post(SPOTIFY_URL_TOKEN, params=body, headers=headers)
-    return handleToken(json.loads(post.text))
-    
-def handleToken(response):
-    auth_head = {"Authorization": "Bearer {}".format(response["access_token"])}
-    REFRESH_TOKEN = response["refresh_token"]
-    return [response["access_token"], auth_head, response["scope"], response["expires_in"]]
 
-def refreshAuth():
-    body = {
-        "grant_type" : "refresh_token",
-        "refresh_token" : REFRESH_TOKEN
-    }
+    def refreshToken(self, time):
+        time.sleep(time)
+        self.token_data = self.refreshAuth()
 
-    post_refresh = requests.post(SPOTIFY_URL_TOKEN, data=body, headers=HEADER)
-    p_back = json.dumps(post_refresh.text)
-    
-    return handleToken(p_back)
+
